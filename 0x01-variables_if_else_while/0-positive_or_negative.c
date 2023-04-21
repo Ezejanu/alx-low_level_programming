121f6d55e109caaf0d6234ff3cece9dbf4075887
Ezejanu <Ezejanu_nno@ymail.com>
2023-04-21 16:05:33 -0700

    corrections to task0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,6 +4,7 @@
 
 /**
  *  Main - Entry point
+ *
  *  If, else code in order to print whether the number
  *  stored in the variable n is positive or negative
  *  Return: Always 0 (Success)
@@ -20,6 +21,6 @@ int main(void)
 	else if (n < 0)
 	printf("%d is negative\n", n);
 	else
-	printf("%d zero\n", n);
+	printf("%d is zero\n", n);
 	return (0);
 }
