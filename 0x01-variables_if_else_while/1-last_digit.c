cfc15c4fa470f07447a8a04b3f57646adf6527a5
Ezejanu <Ezejanu_nno@ymail.com>
2023-04-21 17:06:47 -0700

    corrections to task1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,15 +15,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (LD == 0)
 	{
-		printf("LD of %d is %d and is 0\n" LD, n);
+		printf("LD of %d is %d and is 0\n", n, LD);
 	}
 		else if (LD > 5)
 	{	
-			printf("LD of %d is %d and is greater than 5\n" LD, n);
+			printf("LD of %d is %d and is greater than 5\n", n, LD);
 	}
 		else if (LD > 0 < 6)
 	{
-			printf("LD of %d is %d and is less than 6 and not 0\n" LD, n);
+			printf("LD of %d is %d and is less than 6 and not 0\n", n, LD);
 	}
 	return (0);
 }
