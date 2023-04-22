c93f05e6ef35a9432eab5fabf6196e5acd37d483
Ezejanu <Ezejanu_nno@ymail.com>
2023-04-21 17:37:43 -0700

    corrections to task1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -6,26 +6,28 @@
  * main - Entry point
  * Description: Get a random number and print the number
  * positive,nagetive or zero
- * Return: Always 0 (success)  
+ * Return: Always 0 (success)
  */
 int main(void)
 
 {
-	int n; int ld;
+	int n;
+	int ld;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	ld = n % 10;
 		if (ld == 0)
 	{
-		printf("ld of %d is %d and is 0\n", n, ld);
+		printf("Last digit of %d is %d and is 0\n", n, ld);
 	}
 		else if (ld > 5)
-	{	
-			printf("ld of %d is %d and is greater than 5\n", n, ld);
+	{
+			printf("Last digit of %d is %d and is greater than 5\n", n, ld);
 	}
 		else if (ld < 6)
 	{
-			printf("ld of %d is %d and is less than 6 and not 0\n", n, ld);
+			printf("Last digit of %d is %d and is less than 6 and not 0\n", n, ld);
 	}
 	return (0);
 }
