7cbf63bc34fe28efb8122e026fabf9dcfe2a7be9
Ezejanu <Ezejanu_nno@ymail.com>
2023-04-23 13:19:20 -0700

    lowercase alphabets in reverse

---
+++ 0x01-variables_if_else_while/6-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry
+ *
+ * Description: This is a program that prints the alphabet in lowercase,
+ *and then in uppercase, followed by a new line.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'z'; ch >= 'a'; ch--)
+	putchar (ch);
+
+	putchar ('\n');
+	return (0);
