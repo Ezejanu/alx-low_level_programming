a9d27c673d32b1ac50e8baf51daef59ce98095c6
Ezejanu <Ezejanu_nno@ymail.com>
2023-04-22 21:25:00 -0700

    lowercase and uppercase alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,12 +12,12 @@ int main(void)
 {
 	int ch;
 
-	for (ch = 'a'; <= 'z'; ch++)
+	for (ch = 'a'; ch <= 'z'; ch++)
 		putchar(ch);
 
-	for (ch = 'A'; <= 'Z'; ch++)
+	for (ch = 'A'; ch <= 'Z'; ch++)
 		putchar(ch);
-	putchar('\n')
+	putchar('\n');
 
 	return (0);
 }
