11d910f8eabef82ed393fafcfb8626a2538eae3f
Ezejanu <Ezejanu_nno@ymail.com>
2023-04-23 14:41:37 -0700

    lowercase alphabets omitting e and q

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -2,8 +2,7 @@
 #include <ctype.h>
 
 /**
- * main - Entry point
- * This is a program that prints the alphabet in lowercase (omitting q and e),
+ * main - This is a program that prints the alphabet in lowercase (omitting q and e),
  * followed by a new line
  * Return 0 Always: (Success)
  */
