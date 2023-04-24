5000168cc6c5a45ff355d516eda5430708e1320c
Ezejanu <Ezejanu_nno@ymail.com>
2023-04-23 20:54:58 -0700

    possible combinations of a single digit number

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,11 +11,13 @@ int main(void)
 	int ch;
 
 	for (ch = '0'; ch <= '9'; ch++)
+	{
 		putchar (ch);
-	if (ch != '9')
+		if (ch != '9')
 	{
 		putchar (',');
 		putchar (' ');
+	}
 	}
 
 	return (0);
