bd4d30382426d6ca896875cb1932cd39459ac344
Ezejanu <Ezejanu_nno@ymail.com>
2023-04-23 19:35:05 -0700

    nos of base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <ctype.h>
+/**
+ * main - Entry
+ *
+ * Description: This is a program that prints the numbers of base 16
+ * in lowercase, followed by a new line.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int no;
+	int ch;
+
+	{
+
+		for (ch = 'a'; ch <= 'f'; ch++)
+		putchar (ch);
+	}
+		for (no = '1'; no <= '9' no++)
+	{
+		putchar (no);
+
+		putchar ('\n');
+
+		return (0);
+	}
+}
