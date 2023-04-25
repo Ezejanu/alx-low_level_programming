fce779a0ee933ae82e4a0ce25978669ec0a88513
Ezejanu <Ezejanu_nno@ymail.com>
2023-04-25 08:24:28 -0700

    the alphabet, in lowercase

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,15 +5,16 @@
  *
  * in lowercase, followed by a new line
  *
- * Return: Always 0.
+ * Return: void.
  */
-int main(void)
+void print_alphabet(void)
 {
 	char alph;
 
 	for (alph = 'a'; alph <= 'z'; alph++)
+	{
 		_putchar(alph);
+	}
 	_putchar('\n');
 
-	return (0);
 }
