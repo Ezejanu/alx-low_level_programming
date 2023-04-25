ad504774a7f117d7a7b140db3a139940b1f4b504
Ezejanu <Ezejanu_nno@ymail.com>
2023-04-25 07:43:59 -0700

    program that prints _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,18 +8,17 @@
 
 int main(void)
 {
-	int _putchar;
 
-	_putchar ('_');
-	_putchar ('p');
-	_putchar ('u');
-	_putchar ('t');
-	_putchar ('c');
-	_putchar ('h');
-	_putchar ('a');
-	_putchar ('r');
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 
-	putchar ('\n');
+	putchar('\n');
 	return (0);
 }
 
