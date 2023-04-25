5ab021515f22780738aa249f84c889eca7923bde
Ezejanu <Ezejanu_nno@ymail.com>
2023-04-25 10:54:07 -0700

    prints 10 times the alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -11,14 +11,14 @@ void print_alphabet_x10(void)
 {
 	char alph;
 	int no = 10;
-	{
+
 	while (no < 10)
-		
+	{
 		for (alph = 'a'; alph <= 'z'; alph++)
-
+		{
 		_putchar(alph);
+		}
 		_putchar('\n');
-
 		no++;
 	}
 }
