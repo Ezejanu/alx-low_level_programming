373eefe1cf3074ce7350884bddc8bb5407412fd0
Ezejanu <Ezejanu_nno@ymail.com>
2023-04-25 07:37:07 -0700

    program that prints _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include <main.h>
+/**
+ * main - Description: this is a program that prints _putchar
+ *
+ * followed by a new line.
+ * Return 0 always (success)
+ */
+
+int main(void)
+{
+	int putchar;
+
+	putchar ('_');
+	putchar ('p');
+	putchar ('u');
+	putchar ('t');
+	putchar ('c');
+	putchar ('h');
+	putchar ('a');
+	putchar ('r');
+
+	putchar ('\n');
+	return (0);
+}
+
