98834cd436b9bce5a3e251e4faadefa020fcc3b1
Ezejanu <Ezejanu_nno@ymail.com>
2023-04-26 17:43:46 -0700

    9 times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,21 +10,17 @@ void times_table(void)
 	int c;
 
 		for (a = 0; a <= 9; a++)
-		{
 			_putchar(48);
 			for (b = 1; b <= 9; b++)
-			{
+
 				_putchar(',');
 				_putchar(' ');
 				c = a * b;
 				if (c <= 9)
 					_putchar(' ');
 				else
-				{
 					_putchar((c / 10) + 48);
 					_putchar((c % 10) + 48);
-				}
-			}
+
 	_putchar('\n');
-		}
 }
