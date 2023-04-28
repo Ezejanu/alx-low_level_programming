31297b3cc29b621a6c564518bb097811ff7e87b4
Ezejanu <Ezejanu_nno@ymail.com>
2023-04-28 06:42:52 +0100

    print numbers 0 to 9 except 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,10 +11,10 @@ void print_most_numbers(void)
   char c;
 
   for (c = '0'; c <= '9'; c++)
-    _putchar(c);
   {
   if (c == '2' || c == '4')
     continue;
+  _putchar(c);
   }
   _putchar('\n');
 }
