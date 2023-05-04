f3e6cd6c890d339eda728aa6dfae7e04ed317c05
Ezejanu <Ezejanu_nno@ymail.com>
2023-05-04 08:47:29 -0700

    reverse a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,14 +8,31 @@
 
 void rev_string(char *s)
 {
+	int ls = 0; /* ls is length of the string */
+	int mp = (ls / 2); /* mp is the midpoint of the string */
+	int m; /* m is used as the marker */
+
 	while (*s != '\0')
 	{
 		s++;
+		ls++;
+			/**
+			* ls is also counting so we can use it in the code below.
+			* Using s in the code below gives the error
+			* 'comparison between pointer and integer'
+			*/
 	}
-	s--;
-	while (*s != '\0')
+
+	for (m = 0; m < ls; m++)
+		m--;
+
+	for (m = 0; m < mp; m++)
 	{
-		s--;
+		int last = ls - 1;
+
+		char last_char = s[last - m];
+
+		s[last - m] = s[m];
+		s[m] = last_char;
 	}
-	_putchar(*s);
 }
