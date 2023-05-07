36b70c029b54e0c80dde4a6815671ccc295ab396
Ezejanu <Ezejanu_nno@ymail.com>
2023-05-07 12:47:49 -0700

    capitalise words and leet encoding

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -7,6 +7,22 @@
  *
  */
 
-char *leet(char *s);
+char *leet(char *s)
 {
+	int a, b;
+	char slet[] = "aAeEoOtTlL";
+	char snum[] = "4433007711";
+
+	for (a = 0; s[a] != '\0'; a++)
+	{
+		for (b = 0; b < 10 ; b++)
+		{
+			if (s[a] == slet[b])
+			{
+				s[a] = snum[b];
+			}
+		}
+	}
+	return (s);
+}
 
