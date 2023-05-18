c3427c8e0682a7c505a5c44992a0bc3ce401c273
Ezejanu <Ezejanu_nno@ymail.com>
2023-05-17 17:35:24 -0700

    task 3- alloc_grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -7,8 +7,36 @@
 * @width: width of array
 * @height: height of array
 * Return: pointer to the array
+*/
 
 int **alloc_grid(int width, int height)
 {
+	int **tdp, a = 0, b = 0;
+	/* tdp stands for 2 dimensional pointer */
 
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	tdp = (malloc(sizeof(int) * height));
+	if (tdp == NULL)
+		return (NULL);
+
+	for (; a < height; a++)
+	{
+		tdp[a] = (malloc(sizeof(int) * width));
+		if (tdp[a] == NULL)
+		{
+			while (a--)
+				free(tdp[a]);
+			free(tdp);
+			return (NULL);
+		}
+	}
+
+	for (; a < height; a++)
+	{
+		for (; b < width; b++)
+		tdp[a][b] = 0;
+	}
+	return (tdp);
 }
