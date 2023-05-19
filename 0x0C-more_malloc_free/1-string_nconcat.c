9cc32aa44abd5523bf1087550bfa2feb7909d74c
Ezejanu <Ezejanu_nno@ymail.com>
2023-05-18 17:27:51 -0700

    malloc

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* string_nconcat - a function that concatenates two strings.
+* @s1: string 1
+* @s2: string 2
+* @n: size of bytes
+* Return: pointer shall point to a newly allocated space in memory
+* return NULL if the function fails
+*/
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int size1, size2, a = 0;
+	char *str;
+
+	if (s1 == NULL)
+		s1 = "\0";
+	if (s2 == NULL)
+		s2 = "\0";
+
+	size1 = size2 = 0;
+
+	for (; s1[size1] != '\0'; size1++)
+	for (; s2[size2] != '\0' && size2 < n; size2++)
+
+	str = malloc(size1 + n + 1);
+	if (str == NULL)
+		return (NULL);
+
+	for (; a < size1; a++)
+		str[a] = s1[a];
+	for (; a < size1 + size2; a++)
+		str[a] = s2[a - size1];
+
+	str[a] = '\0';
+	return (str);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,3 @@
+A readme file for more taks on malloc.
+There are three (3) mandatory tasks in this project
+and two (2) advanced tasks.
