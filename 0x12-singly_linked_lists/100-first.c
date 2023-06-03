a708b0d1308717e008760a3e466554b368bce7d0
Ezejanu <Ezejanu_nno@ymail.com>
2023-06-03 12:00:38 -0700

    correction to la lievre et la tortue

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,7 +5,7 @@
 * before_main - a function that executes before the main function
 */
 
-void before_main(main)
+void before_main(void)
 {
 
 	printf("You're beat! and yet, you must allow,\n");
--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -4,8 +4,8 @@
 	extern printf
 
 main:
-	mov edi, format
-	xor eax, eax
-	call printf
-	xor eax, eax
-	ret
+	mov edi, format ; Move the address of the format string into the edi register
+	xor eax, eax; Set eax to 0 to indicate successful execution
+	call printf ; Call printf with the address of the format string as the argument
+	xor eax, eax ; Set eax to 0 (optional, for clarity)
+	ret ; Return control to the operating system
