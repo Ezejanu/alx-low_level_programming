3730d0d1df28bbfe17b3510f0c904423b47e248b
Ezejanu <Ezejanu_nno@ymail.com>
2023-06-02 22:09:25 -0700

    a 64-bit program in assembly

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -4,8 +4,8 @@
 	extern printf
 
 main:
-	push format; Push the address of the format string onto the stack
-	call printf; Call printf with the address of the format string as the argument
-	add esp, 4; Adjust the stack pointer to remove the pushed argument
-	xor eax, eax; Set eax to 0 to indicate successful execution
-	ret; Return control to the operating system
+	mov edi, format
+	xor eax, eax
+	call printf
+	xor eax, eax
+	ret
