4cb5f0014659405d3ca0fe4f3877d00a47fc6865
Ezejanu <Ezejanu_nno@ymail.com>
2023-06-06 11:58:50 -0700

    insert a node in a singly linked list

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -33,13 +33,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	unsigned int count = 0;
 
-	listint_t *temp;
+	listint_t *temp, *newNode;
 
 	if (idx > nodeSize(*head) || head == NULL || *head == NULL)
 		return (NULL);
 
 	temp = *head;
-/**	if (idx == 0)
+	if (idx == 0)
 	{
 		listint_t *newNode = malloc(sizeof(listint_t));
 
@@ -49,13 +49,13 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		newNode->next = *head;
 		*head = newNode;
 		return (newNode);
-	} */
+	}
 	while (temp != NULL)
 	{
 		count++;
 		if (count == idx)
 		{
-			listint_t *newNode = (listint_t *) malloc(sizeof(listint_t));
+			newNode = (listint_t *) malloc(sizeof(listint_t));
 
 			if (newNode == NULL)
 				return (NULL);
@@ -65,6 +65,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 			return (newNode);
 		}
 		temp = temp->next;
+
 	}
 	return (NULL);
 }
