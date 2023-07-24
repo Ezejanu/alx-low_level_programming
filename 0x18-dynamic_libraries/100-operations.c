d478932c643f9600348c57d0ed6a06da7b9f60a7
Ezejanu <Ezejanu_nno@ymail.com>
2023-07-24 16:42:07 -0700

    task 2- a dynamic library that contains C functions that can be called from Python

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,72 @@
+int _add(int a, int b);
+int _sub(int a, int b);
+int _mul(int a, int b);
+int _div(int a, int b);
+int _mod(int a, int b);
+
+/**
+ * _add - a function to add a and b
+ * @a: int 1
+ * @b: int 2
+ * Return: result
+ */
+
+int _add(int a, int b)
+{
+	return (a + b);
+}
+
+
+/**
+ * _sub - a function to subtract a and b
+ * @a: int 1
+ * @b: int 2
+ * Return: result
+ */
+
+int _sub(int a, int b)
+{
+	return (a - b);
+}
+
+
+
+/**
+ * _mul - a function to multiply a and b
+ * @a: int 1
+ * @b: int 2
+ * Return: result
+ */
+
+int _mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+
+/**
+ * _div - a function to divide a and b
+ * @a: int 1
+ * @b: int 2
+ * Return: result
+ */
+
+int _div(int a, int b)
+{
+	return (a / b);
+}
+
+
+
+/**
+ * _mod - a function to find the modular of a and b
+ * @a: int 1
+ * @b: int 2
+ * Return: result
+ */
+
+int _mod(int a, int b)
+{
+	return (a % b);
+}
