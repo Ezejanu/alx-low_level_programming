bac1674522c4efec681f13e840d8402fdc97c610
Ezejanu <Ezejanu_nno@ymail.com>
2023-07-24 16:52:41 -0700

    checking task 2

--- 0x18-dynamic_libraries/100-operations.c
+++ 0x18-dynamic_libraries/100-operations.c
@@ -1,8 +1,4 @@
-int _add(int a, int b);
-int _sub(int a, int b);
-int _mul(int a, int b);
-int _div(int a, int b);
-int _mod(int a, int b);
+#include "_ops.h"
 
 /**
  * _add - a function to add a and b
