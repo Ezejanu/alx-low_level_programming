0a52921a7f0d47f0b0dcf0ea754a83a000d510f8
Ezejanu <Ezejanu_nno@ymail.com>
2023-08-11 14:09:15 -0700

    task 4- checking for head or ht == NULL

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -14,14 +14,19 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index = 0;
 	hash_node_t *node = NULL;
 
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (NULL);
+
 	index = key_index((const unsigned char *)key, ht->size);
 	node = ht->array[index];
-
-	if (node != NULL)
+	while (node != NULL)
 	{
-		if (strcmp(node->key, key) == 0)
-			return (node->value);
+		if (node != NULL)
+		{
+			if (strcmp(node->key, key) == 0)
+				return (node->value);
+		}
+		node = node->next;
 	}
-
 	return (NULL);
 }
