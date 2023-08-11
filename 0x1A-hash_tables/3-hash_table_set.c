2994901fc558b60a9ce8249e953aca7ca4db732d
Ezejanu <Ezejanu_nno@ymail.com>
2023-08-11 11:28:22 -0700

    task 3 - included check for if key is an empty string

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -17,6 +17,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node = NULL;
 	unsigned long int new_node_index = 0;
 
+	if (key == NULL)
+		return (0);
+
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
